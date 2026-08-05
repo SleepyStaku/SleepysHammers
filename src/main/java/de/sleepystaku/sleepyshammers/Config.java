5c2425af60c9db5d21390c4015a413c36f55538a
SleepyStaku <EthanFranken01@outlook.com>
2026-08-05 11:58:17 +0200

    Initial commit

---
+++ src/main/java/de/sleepystaku/sleepyshammers/Config.java
@@ -0,0 +1,10 @@
+package de.sleepystaku.sleepyshammers;
+
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+// An config class. This is not required, but it's a good idea to have one to keep your config organized.
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    static final ModConfigSpec SPEC = BUILDER.build();
+}
