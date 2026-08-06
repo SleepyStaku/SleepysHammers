7712f4e339b47d03c3b6239e1d04825d9e06bc51
SleepyStaku <EthanFranken01@outlook.com>
2026-08-06 10:40:07 +0200

    Added HammerItems support

--- src/main/java/de/sleepystaku/sleepyshammers/item/ModItems.java
+++ src/main/java/de/sleepystaku/sleepyshammers/item/ModItems.java
@@ -11,25 +11,25 @@ public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(SleepysHammers.MODID);
 
     public static final DeferredItem<Item> WOODEN_HAMMER = ITEMS.registerItem("wooden_hammer", properties ->
-            new Item(properties.pickaxe(ToolMaterial.WOOD, 3, -2.8f)));
+            new HammerItem(properties.pickaxe(ToolMaterial.WOOD, 3, -2.8f)));
 
     public static final DeferredItem<Item> STONE_HAMMER = ITEMS.registerItem("stone_hammer", properties ->
-            new Item(properties.pickaxe(ToolMaterial.STONE, 4, -2.8f)));
+            new HammerItem(properties.pickaxe(ToolMaterial.STONE, 4, -2.8f)));
 
     public static final DeferredItem<Item> COPPER_HAMMER = ITEMS.registerItem("copper_hammer", properties ->
-            new Item(properties.pickaxe(ToolMaterial.COPPER, 4, -2.8f)));
+            new HammerItem(properties.pickaxe(ToolMaterial.COPPER, 4, -2.8f)));
 
     public static final DeferredItem<Item> IRON_HAMMER = ITEMS.registerItem("iron_hammer", properties ->
-            new Item(properties.pickaxe(ToolMaterial.IRON, 4, -2.8f)));
+            new HammerItem(properties.pickaxe(ToolMaterial.IRON, 4, -2.8f)));
 
     public static final DeferredItem<Item> GOLDEN_HAMMER = ITEMS.registerItem("golden_hammer", properties ->
-            new Item(properties.pickaxe(ToolMaterial.GOLD, 3, -2.8f)));
+            new HammerItem(properties.pickaxe(ToolMaterial.GOLD, 3, -2.8f)));
 
     public static final DeferredItem<Item> DIAMOND_HAMMER = ITEMS.registerItem("diamond_hammer", properties ->
-            new Item(properties.pickaxe(ToolMaterial.DIAMOND, 4, -2.8f)));
+            new HammerItem(properties.pickaxe(ToolMaterial.DIAMOND, 4, -2.8f)));
 
     public static final DeferredItem<Item> NETHERITE_HAMMER = ITEMS.registerItem("netherite_hammer", properties ->
-            new Item(properties.pickaxe(ToolMaterial.NETHERITE, 4, -2.8f)));
+            new HammerItem(properties.pickaxe(ToolMaterial.NETHERITE, 4, -2.8f)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
